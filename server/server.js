a017686fa4a9b370d362fc3d63f17ceba1209f22
sornya <sornyaa9@hotmail.com>
2023-07-26 14:50:21 -0400

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 app.use(express.static(`public`))//middleware every request will go to static
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname,'../public/index.html'))
-// })
+ app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname,'../public/index.html'))
+ })
 app.listen(4000, () => {
     console.log(`server is runing on 4000`)
 })
