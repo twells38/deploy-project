e9d7e10ccc208bcda9d01f8a626f05886dd20b52
sornya <sornyaa9@hotmail.com>
2023-07-26 12:54:39 -0400

    second commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-app.use(express.static(`${__dirname}/public`))//middleware every request will go to static
-
+const path = require('path')
+app.use(express.static(`public`))//middleware every request will go to static
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,'../public/index.html'))
+// })
 app.listen(4000, () => {
     console.log(`server is runing on 4000`)
 })
